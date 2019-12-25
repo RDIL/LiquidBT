e3ef622b04a9eec64ba599325434bcb9d0d648f4
Reece Dunham <me@rdil.rocks>
2019-12-25 01:40:23 +0000

    deploy plugin revamp

--- liquidbt_plugin_deploy/__init__.py
+++ liquidbt_plugin_deploy/__init__.py
@@ -1,9 +1,11 @@
 from liquidbt.plugins import Plugin
 import typing
+import os
+import sys
 
 
 class Deploy(Plugin):
-    def __init__(self, package, repo, **kwargs) -> None:
+    def __init__(self, package, repo, **kwargs):
         self.kwargs = kwargs
 
     @property
@@ -13,20 +15,25 @@ class Deploy(Plugin):
             "deploy": self.entrypoint
         }
 
-    def entrypoint(self, plugins):
+    def entrypoint(self, repository, plugins):
         return
 
 
+def use_environment():
+    return (os.getenv("TWINE_USERNAME"), os.getenv("TWINE_PASSWORD"))
+
+
 class Repository:
-    def __init__(self, name, *args, **kwargs) -> None:
-        self._name = name
-        self.args = args
+    def __init__(self, credentials, **kwargs) -> None:
+        self._credentials = credentials
         self.kwargs = kwargs
 
     @property
-    def name(self) -> typing.Union[str, None]:
-        return self._name
-
-    @property
     def url(self) -> typing.Union[str, None]:
-        return self.kwargs.get("url", None)
+        return self.kwargs.get("url", "https://upload.pypi.org/legacy/")
+
+    def upload(self):
+        repo = f"--repository-url {self.url}"
+        creds = f"-u {self._credentials[0]} -p {self._credentials[1]}"
+        cmd = "python3" if sys.platform != "nt" else "python"
+        os.system(f"{cmd} -m twine upload dist/* {repo} {creds}")
